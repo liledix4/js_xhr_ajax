51d821fd4f44334bd1a8310224af452b792885e7
liledix4 <liledix4@musician.org>
2025-02-08 11:56:29 +0000

    0.3.1 - No more dependency from "js_mime_presets" module

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
 # js_xhr_ajax
 
-XMLHttpRequest/AJAX for JavaScript projects
-
-## Requirements
-
-You must also clone [`js_mime_presets`](https://github.com/liledix4/js_mime_presets) next to the cloned directory of this repository, like this:
-
-```
-your_directory/
-├── js_mime_presets/
-└── js_xhr_ajax/
-```
+XMLHttpRequest/AJAX for JavaScript projects.
--- xhr_ajax.js
+++ xhr_ajax.js
@@ -1,1 +1,115 @@
-import{mime}from"../js_mime_presets/mime_presets.js";function xhrDefaults(e){return e.method||(e.method="GET"),e.mime||(e.mime=mime.html),e.async||(e.async=!0),e.contentCharset||(e.contentCharset="UTF-8"),e.contentType||(e.contentType=`${e.mime}; charset=${e.contentCharset}`),e}export function readTextFile(e,t,s=null){e=xhrDefaults(e);const n=new XMLHttpRequest;n.open(e.method,e.url,e.async),n.onerror=function(){},n.setRequestHeader("Content-Type",e.contentType),e.headers&&e.headers.forEach((e=>{void 0!==e&&n.setRequestHeader(e.name,e.value)})),n.onreadystatechange=function(){4===n.readyState&&("200"==n.status&&t?t(n.responseText):t({status:n.status,response:n.responseText}))},n.send(s)}
+/**
+ *
+ *
+ * @param {object} xhrRequest
+ * @return {object} Same `xhrRequest` object, but with additional data that was missing.
+ * @since 0.1
+ */
+function xhrDefaults(xhrRequest) {
+    if (!xhrRequest.method) {
+        xhrRequest.method = 'GET';
+    }
+    if (!xhrRequest.mime) {
+        xhrRequest.mime = 'application/x-www-form-urlencoded';
+    }
+    if (!xhrRequest.async) {
+        xhrRequest.async = true;
+    }
+    if (!xhrRequest.contentCharset) {
+        xhrRequest.contentCharset = 'UTF-8';
+    }
+    if (!xhrRequest.contentType) {
+        xhrRequest.contentType = `${xhrRequest.mime}; charset=${xhrRequest.contentCharset}`;
+    }
+    return xhrRequest;
+}
+
+/**
+ *  ----
+ *
+ *  @example
+ *  // Shortest
+ *  readTextFile( { url: './file.json' }, content => { console.log( content ) } );
+ *
+ *  // More complex
+ *  readTextFile(
+ *      {
+ *          url: './path/to/file.json',
+ *          method: 'GET',
+ *          mime: mime.json
+ *      },
+ *      fileContent => {
+ *          console.log( fileContent );
+ *      }
+ *  );
+ *  @param {{
+ *      url: String,
+ *      headers?: Array.<{
+ *          name: String,
+ *          value: String
+ *      }>,
+ *      method?: String,
+ *      mime?: String,
+ *      async?: Boolean,
+ *      contentCharset?: String,
+ *      contentType?: String
+ *  }} xhrRequest
+ *  | Required | Object key | Purpose |
+ *  | -------: | :--------- | :------ |
+ *  | ⚠️ | `xhrRequest.url` | Path to the file you get using XMLHttpRequest |
+ *  |   | `xhrRequest.headers[]` | Array with [HTTP headers](https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/setRequestHeader) for sending to the server. By default, there are none |
+ *  | 🟢 | `xhrRequest.headers[].name` | Name of HTTP header |
+ *  | 🟢 | `xhrRequest.headers[].value` | Value of HTTP header |
+ *  |   | `xhrRequest.method` | [HTTP request method](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods). Default is `GET` |
+ *  |   | `xhrRequest.mime` | [MIME type](https://www.iana.org/assignments/media-types/media-types.xhtml). *MIME presets* can be used. Default is `application/x-www-form-urlencoded` |
+ *  |   | `xhrRequest.async` | [Asynchronous performing of operation](https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/open#async). Default is `true` |
+ *  |   | `xhrRequest.contentCharset` | Content charset. Default is `UTF-8` |
+ *  |   | `xhrRequest.contentType` | A combination of MIME type and content charset, however you can customize it to your needs |
+ *  @param {() => void} callback Your custom function that is executed after successful fetching of data
+ *  @returns {void}
+ *  @description Get the contents of file or perform other operations using XMLHttpRequest.
+ *
+ *  ----
+ *
+ *  @author [liledix4 🎸](https://liledix4.github.io)
+ *  @version 0.3.1
+ *  @license Apache-2.0
+ */
+export function readTextFile(xhrRequest, callback, send = null) {
+    xhrRequest = xhrDefaults(xhrRequest);
+    const xhr = new XMLHttpRequest(); // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/XMLHttpRequest
+
+    xhr.open( // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/open
+        xhrRequest.method,  // method
+        xhrRequest.url,     // url
+        xhrRequest.async    // async
+    );
+    xhr.onerror = function() {
+        console.error(failureMessage);
+    }
+    xhr.setRequestHeader('Content-Type', xhrRequest.contentType);
+    if (xhrRequest.headers) {
+        xhrRequest.headers.forEach(header => {
+            if (header !== undefined) {
+                xhr.setRequestHeader(
+                    header.name,
+                    header.value
+                );
+            }
+        });
+    }
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status == '200' && callback)
+                callback(xhr.responseText);
+            else
+                callback(
+                    {
+                        status: xhr.status,
+                        response: xhr.responseText,
+                    }
+                );
+        }
+    }
+    xhr.send(send); // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/send
+}
